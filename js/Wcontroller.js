4ba928eeb0af470c446f0aa30a8ead6944b45787
Mostafa Habib <MostafaSHabib@gmail.com>
2015-09-08 11:29:47 -0400

    basic angular stuff working, main controller and appJS work and speak to each other

---
+++ js/Wcontroller.js
@@ -0,0 +1,12 @@
+//dependency injection for AJAX calls
+
+// app.controller('WeatherController', ['$http', function($http){
+
+
+  // var weather = this;
+
+  //get request
+//   $http.get('http://api.wunderground.com/api/9a5c4fd309b43a2a/conditions/q/CA/San_Francisco.json', {apikey: '9a5c4fd309b43a2a'}).success(function(data) {
+//     return data;
+//   })
+// } ]);
