0bc0833e80980b4055f6dc44bd17b3af29c6f955
Mostafa Habib <MostafaSHabib@gmail.com>
2015-09-08 13:30:16 -0400

    AJAX request successful, and data shows up on page

--- js/MainController.js
+++ js/MainController.js
@@ -1,3 +1,4 @@
+// linking title with the $scope service and injecting it into the controller
 app.controller('MainController', ['$scope', function($scope) {
   $scope.title = "Weather Bitches";
 }])
